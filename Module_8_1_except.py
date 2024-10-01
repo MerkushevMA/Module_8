7492a94455de2eebac283a3a624c17556b706f78
MerkushevMA <s2dent59@gmail.com>
2024-10-01 21:44:04 +0500

    Update Module_8_1_except.py

--- Module_8_1_except.py
+++ Module_8_1_except.py
@@ -2,7 +2,7 @@ def add_everything_up(a, b):
     try:
         result = a + b
     except TypeError:
-        result = a, b
+        result = f'{a}{b}'
     return result
 
 print(add_everything_up(123.456, 'строка'))
